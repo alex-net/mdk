b9f055acb0c267d527fd6f278e80290c215a350b
alex-net <alexnet2004@gmail.com>
2012-09-20 00:00:05 +0300

    Делаем блоки ... пытаюсь применить линейные градиенты ..

--- html/js/my.js
+++ html/js/my.js
@@ -2,10 +2,10 @@ jQuery(function(){
 	if (window.PIE)
 	{
 		// используем файлик htc 
-	/*	jQuery('.site-head-menu div').each(function(){
+		jQuery('.site-blocks .b-head,  .site-head-menu div, .site-head-menu').each(function(){
 			PIE.attach(this);
 			//alert('s');
-		});*/
+		});
 		
 	}
 	//alert();
