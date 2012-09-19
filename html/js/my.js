0f1101ccdecebc43b84a0a8a6fb9d618ad1ebbde
alex-net <alexnet2004@gmail.com>
2012-09-19 12:35:40 +0300

    Фиксим изменения .. которые не пофиксили вчера ... PIE Запустили ... всё классно .. будем дальше верстать наверное стоит дома поставить ie9 .. чтобы тестить во всех браузерах ..

---
+++ html/js/my.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+	if (window.PIE)
+	{
+		// используем файлик htc 
+	/*	jQuery('.site-head-menu div').each(function(){
+			PIE.attach(this);
+			//alert('s');
+		});*/
+		
+	}
+	//alert();
+	
+});
