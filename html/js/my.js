e20989b9fcdf732831d0c6507082da855ba9f311
alex-net <alexnet2004@gmail.com>
2012-09-21 00:40:45 +0300

    Доверстал боковушки на главной странице . в принципе они будут на всех страницах . так что переделывать повторно надеюсь не прийдётся

--- html/js/my.js
+++ html/js/my.js
@@ -2,7 +2,7 @@ jQuery(function(){
 	if (window.PIE)
 	{
 		// используем файлик htc 
-		jQuery('.site-blocks .b-head,  .site-head-menu div, .site-head-menu').each(function(){
+		jQuery('.block-catalog .catalog-content,.block-shadow,.block-shadow-r, .site-blocks .b-head,  .site-head-menu div, .site-head-menu').each(function(){
 			PIE.attach(this);
 			//alert('s');
 		});
