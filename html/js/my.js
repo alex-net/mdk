07702fcc01bcf8c8d1b95db8abf6ce49d1e77bd6
alex-net <alexnet2004@gmail.com>
2012-09-22 02:19:18 +0300

    Закончил вёрстку главной страницы ... на её основе за выходные нужно будет сделать все остальные страницы ...

--- html/js/my.js
+++ html/js/my.js
@@ -2,12 +2,22 @@ jQuery(function(){
 	if (window.PIE)
 	{
 		// используем файлик htc 
-		jQuery('.block-catalog .catalog-content,.block-shadow,.block-shadow-r, .site-blocks .b-head,  .site-head-menu div, .site-head-menu').each(function(){
+		jQuery('.block-catalog .catalog-content,.block-shadow,.block-shadow-r, .site-blocks .b-head,  .site-head-menu div, .site-head-menu, .filter-container .input-data div select, .filter-container .gogo input, .titl-page-and-filter,.tovar-list ul li,.page-content-wrapper').each(function(){
 			PIE.attach(this);
 			//alert('s');
 		});
 		
 	}
+	jQuery('.param-filter-link a').click(function(){
+		if (window.PIE)
+			PIE.detach(jQuery('.titl-page-and-filter')[0]);
+		jQuery('.filter-container').slideToggle(500);
+		//jQuery.delay(500);
+		
+		if (window.PIE)
+			setTimeout("PIE.attach(jQuery('.titl-page-and-filter')[0]);",500);
+			
+	});
 	//alert();
 	
 });
