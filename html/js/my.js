f6db5f56df6a44703263cd1e8736cbe91185abb7
alex-net <alexnet2004@gmail.com>
2012-09-25 08:20:28 +0300

    Доделал вёрству .. теперь надо будет собрать тему ..

--- html/js/my.js
+++ html/js/my.js
@@ -2,7 +2,7 @@ jQuery(function(){
 	if (window.PIE)
 	{
 		// используем файлик htc 
-		jQuery('.block-catalog .catalog-content,.block-shadow,.block-shadow-r, .site-blocks .b-head,  .site-head-menu div, .site-head-menu, .filter-container .input-data div select, .filter-container .gogo input, .titl-page-and-filter,.tovar-list ul li,.page-content-wrapper').each(function(){
+		jQuery('.block-catalog .catalog-content,.block-shadow,.block-shadow-r, .site-blocks .b-head,  .site-head-menu div, .site-head-menu, .filter-container .input-data div select, .filter-container .gogo input, .titl-page-and-filter,.tovar-list ul li,.page-content-wrapper, .cart-item-count input,.cart-item-img, .checkout-form-wrapper, .checkout-form-wrapper input, .checkout-form-wrapper select, .checkout-form-wrapper textarea, .cart-to-checkout input, .go-to-cart input').each(function(){
 			PIE.attach(this);
 			//alert('s');
 		});
@@ -16,7 +16,7 @@ jQuery(function(){
 		
 		if (window.PIE)
 			setTimeout("PIE.attach(jQuery('.titl-page-and-filter')[0]);",500);
-			
+		return false;	
 	});
 	//alert();
 	
